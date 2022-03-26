0bed96d7f3e7cf2e57294ced4cde5965b0380357
Allen Pocket <AllenPocketGamer@Gmail.com>
2022-03-26 11:34:16 +0800

    :sparkles: ($PROJ) add alert-panel shortcodes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./templates/*.html", "./content/**/*.md"],
+  content: ["./templates/**/*.html", "./content/**/*.md"],
   theme: {
     extend: {
       fontFamily: {
